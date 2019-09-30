d6bb794eeda33416b594b18a86d11ceee39e969b
Selamat Purba <selamatpurba@Btpns-MacBook-Pro.local>
2019-09-30 15:04:38 +0700

    fix path vendor and screencapture

--- index.php
+++ index.php
@@ -28,10 +28,10 @@ if( $status == "200" ){
   <link rel="icon" href="/assets/images/favicon2.ico">
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
@@ -289,14 +289,14 @@ if( $status == "200" ){
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/vendor/jquery/jquery.min.js"></script>
+  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="assets/js/grayscale.min.js"></script>
+  <script src="/assets/js/grayscale.min.js"></script>
   
   <script src="/assets/js/Chart.min.js"></script>
   <script src="/assets/js/id.js"></script>
