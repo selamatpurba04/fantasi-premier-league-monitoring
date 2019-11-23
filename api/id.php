ac07ef9e31c696efab2282e4b9e1915aeb8cabbc
selamatpurba04 <adicoy46@yahoo.co.id>
2019-11-23 22:18:07 +0700

    update temp base data

--- api/id.php
+++ api/id.php
@@ -72,4 +72,4 @@ $upImg = "<img src='/assets/images/up-arrow.png' width='12'>";
 $downImg = "<img src='/assets/images/down-arrow.png' width='12'>";
 $stayImg = "<img src='/assets/images/stay-arrow.png' width='12'>";
 
-$currentGW = 12;
+$currentGW = 13;
