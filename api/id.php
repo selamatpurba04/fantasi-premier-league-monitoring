a82b3d22876a81f2b11ea90c390f8f80e10e93ac
Selamat Purba <selamat.purba@btpn.com>
2020-01-02 09:35:21 +0700

    update temp base data gw 21

--- api/id.php
+++ api/id.php
@@ -72,5 +72,5 @@ $upImg = "<img src='/assets/images/up-arrow.png' width='12'>";
 $downImg = "<img src='/assets/images/down-arrow.png' width='12'>";
 $stayImg = "<img src='/assets/images/stay-arrow.png' width='12'>";
 
-$currentGW = 20;
-$startGW = 15;
+$currentGW = 21;
+$startGW = 16;
